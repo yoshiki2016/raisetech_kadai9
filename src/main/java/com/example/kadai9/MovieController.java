4e5ef30c0c69392213aa5ecb65d226a9a4f1f57d
Yoshiki Shin'ya <785431ys@gmail.com>
2023-05-30 18:44:29 +0900

    リクエストパラメーターを利用するように変更

--- src/main/java/com/example/kadai9/MovieController.java
+++ src/main/java/com/example/kadai9/MovieController.java
@@ -2,6 +2,7 @@ package com.example.kadai9;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,13 +15,13 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping("/movies/all")
     public List<Movie> movies() {
         return movieService.findAll();
     }
 
-    @GetMapping("/movies?published_year={published_year}")
-    public List<Movie> findMovie(@PathVariable(name = "published_year") int published_year){
+    @GetMapping("/movies")
+    public List<Movie> findMovie(@RequestParam("published_year") int published_year){
         return movieService.findMovie(published_year);
     }
 
