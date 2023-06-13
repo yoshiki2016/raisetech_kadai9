280119e10c98711b1f950da11e9a06b8671324f0
Yoshiki Shin'ya <785431ys@gmail.com>
2023-06-13 20:00:00 +0900

    zonedDateTimeのモック化、timestampのデータ誤り修正

--- src/test/java/com/example/kadai9/integrationtest/MovieRestApiIntegrationTest.java
+++ src/test/java/com/example/kadai9/integrationtest/MovieRestApiIntegrationTest.java
@@ -99,12 +99,14 @@ public class MovieRestApiIntegrationTest {
     void 存在しないIDの映画を取得しようとすると404エラーになること() throws Exception {
         ZonedDateTime zonedDateTime = ZonedDateTime.of(2023, 6, 16, 13, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
         try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)){
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
+
             String response = mockMvc.perform(get("/movies/100"))
                     .andExpect(status().isNotFound())
                     .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
             JSONAssert.assertEquals("""
                {
-                  "timestamp": 2023-06-16T13:00+09:00[Asia/Tokyo]",
+                  "timestamp": "2023-06-16T13:00+09:00[Asia/Tokyo]",
                   "message": "resource not found",
                   "status": "404",
                   "path": "/movies/100",
